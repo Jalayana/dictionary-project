0debea6c3e8797824943449123c105c866cb125b
Jalayana <millerja14@outlook.com>
2025-06-14 18:18:04 +0800

    Added a footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>Dictionary App</h1>
+      <footer>
+        This app was coded by Jalayana Miller and is open-sourced on{" "}
+        <a href="https://github.com/Jalayana/dictionary-project">GitHub</a> and
+        hosted on{" "}
+        <a href="https://flourishing-shortbread-9d1780.netlify.app/">
+          Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
