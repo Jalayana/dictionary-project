fdd9ac64f66f51075f7d75da877780be8d0ad469
Jalayana <millerja14@outlook.com>
2025-06-14 19:53:33 +0800

    created a component with search form and event handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
+import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
-      <footer>
-        This app was coded by Jalayana Miller and is open-sourced on{" "}
-        <a href="https://github.com/Jalayana/dictionary-project">GitHub</a> and
-        hosted on{" "}
-        <a href="https://flourishing-shortbread-9d1780.netlify.app/">
-          Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Dictionary App</h1>
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          This app was coded by Jalayana Miller and is open-sourced on{" "}
+          <a href="https://github.com/Jalayana/dictionary-project">GitHub</a>{" "}
+          and hosted on{" "}
+          <a href="https://flourishing-shortbread-9d1780.netlify.app/">
+            Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
