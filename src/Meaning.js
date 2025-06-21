4099406cf8beecdb0805d176c9d86af04c4bafff
Jalayana <millerja14@outlook.com>
2025-06-21 11:46:17 +0800

    Added extra styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,9 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3 className="part-of-speech">{props.meaning.partOfSpeech}</h3>
+      <h3 className="part-of-speech">
+        <big>{props.meaning.partOfSpeech}</big>
+      </h3>
       <p className="definition">
         <em>
           {props.index + 1}) {props.meaning.definition}
