bb1445e0d69ba0a4509048263e2706fbf15c0c8b
Jalayana <millerja14@outlook.com>
2025-06-21 11:32:53 +0800

    Photo API integration and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,27 @@ import { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     const apiKey = "036afccb5b3e1756f153fo40e081f7t7";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -48,6 +56,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
